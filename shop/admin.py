b212b25cb25311a3e2254690c689c7fa9575121d
theekshanahansa <randina1999@gmail.com>
2024-06-23 20:53:49 +0530

    Create admin.py and update main urls patterns

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Category, Product, Cart, CartItem, Order
+
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Cart)
+admin.site.register(CartItem)
+admin.site.register(Order)
