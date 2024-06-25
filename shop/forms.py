5460ce7886746e474ad51e88ac364e9494116c85
theekshanahansa <100909756+theekshanahansa@users.noreply.github.com>
2024-06-25 10:15:25 +0530

    add styles to login page and Html pages

--- shop/forms.py
+++ shop/forms.py
@@ -6,13 +6,20 @@ from .models import Customer
 
 
 class UserRegistrationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-    shipping_address = forms.CharField(widget=forms.Textarea, required=True)
-    phone_number = forms.CharField(max_length=15, required=True)
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    shipping_address = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}), required=True)
+    phone_number = forms.CharField(max_length=15, required=True,
+                                   widget=forms.TextInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super(UserRegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password1'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password2'].widget.attrs.update({'class': 'form-control'})
 
     def save(self, commit=True):
         user = super().save(commit=False)
